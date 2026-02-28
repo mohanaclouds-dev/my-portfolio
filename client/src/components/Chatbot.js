817afbcc08dffaa7769c95dede96708d83c0f947
mohanaclouds-dev <mohanaclouds@gmail.com>
2026-02-28 21:32:50 +0530

    Update Chatbot.js

--- client/src/components/Chatbot.js
+++ client/src/components/Chatbot.js
@@ -27,7 +27,7 @@ const Chatbot = () => {
 
     try {
       // ✅ Send message to YOUR Backend
-      const res = await axios.post('https://my-portfolio-t4gb.onrender.com/api/chat', { message: userMessage.text });
+      const res = await axios.post('https://mohana-api.onrender.com/api/chat', { message: userMessage.text });
       
       const botMessage = { text: res.data.reply, sender: "bot" };
       setMessages((prev) => [...prev, botMessage]);
